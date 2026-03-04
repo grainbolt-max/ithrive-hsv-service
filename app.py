ce28c3a06366afcaa9c33bea0ee64729390cc97b
grainbolt-max <grainbolt@gmail.com>
2026-03-04 08:52:05 -0600

    Update app.py

--- app.py
+++ app.py
@@ -8,40 +8,42 @@ app = Flask(__name__)
 
 API_KEY = "ithrive_secure_2026_key"
 
+# SINGLE CONTROL FOR BAR POSITION
+BAR_X = 300
 
 BASE_LAYOUT = {
 
     # PAGE 2 – CARDIO / DIABETES
 
-    "large_artery_stiffness": {"x": 430, "y": 110, "w": 520, "h": 42},
-    "peripheral_vessel": {"x": 430, "y": 152, "w": 520, "h": 42},
-    "blood_pressure_uncontrolled": {"x": 430, "y": 194, "w": 520, "h": 42},
-    "small_medium_artery": {"x": 430, "y": 236, "w": 520, "h": 42},
-    "atherosclerosis": {"x": 430, "y": 278, "w": 520, "h": 42},
-    "ldl_cholesterol": {"x": 430, "y": 320, "w": 520, "h": 42},
-    "lv_hypertrophy": {"x": 430, "y": 362, "w": 520, "h": 42},
+    "large_artery_stiffness": {"x": BAR_X, "y": 110, "w": 520, "h": 42},
+    "peripheral_vessel": {"x": BAR_X, "y": 152, "w": 520, "h": 42},
+    "blood_pressure_uncontrolled": {"x": BAR_X, "y": 194, "w": 520, "h": 42},
+    "small_medium_artery": {"x": BAR_X, "y": 236, "w": 520, "h": 42},
+    "atherosclerosis": {"x": BAR_X, "y": 278, "w": 520, "h": 42},
+    "ldl_cholesterol": {"x": BAR_X, "y": 320, "w": 520, "h": 42},
+    "lv_hypertrophy": {"x": BAR_X, "y": 362, "w": 520, "h": 42},
 
-    "metabolic_syndrome": {"x": 430, "y": 440, "w": 520, "h": 42},
-    "insulin_resistance": {"x": 430, "y": 482, "w": 520, "h": 42},
-    "beta_cell_function": {"x": 430, "y": 524, "w": 520, "h": 42},
-    "blood_glucose": {"x": 430, "y": 566, "w": 520, "h": 42},
-    "tissue_inflammation": {"x": 430, "y": 608, "w": 520, "h": 42},
+    "metabolic_syndrome": {"x": BAR_X, "y": 440, "w": 520, "h": 42},
+    "insulin_resistance": {"x": BAR_X, "y": 482, "w": 520, "h": 42},
+    "beta_cell_function": {"x": BAR_X, "y": 524, "w": 520, "h": 42},
+    "blood_glucose": {"x": BAR_X, "y": 566, "w": 520, "h": 42},
+    "tissue_inflammation": {"x": BAR_X, "y": 608, "w": 520, "h": 42},
 
     # PAGE 3 – MISC DISEASES
 
-    "hypothyroidism": {"x": 430, "y": 870, "w": 520, "h": 42},
-    "hyperthyroidism": {"x": 430, "y": 912, "w": 520, "h": 42},
-    "hepatic_fibrosis": {"x": 430, "y": 954, "w": 520, "h": 42},
-    "chronic_hepatitis": {"x": 430, "y": 996, "w": 520, "h": 42},
+    "hypothyroidism": {"x": BAR_X, "y": 870, "w": 520, "h": 42},
+    "hyperthyroidism": {"x": BAR_X, "y": 912, "w": 520, "h": 42},
+    "hepatic_fibrosis": {"x": BAR_X, "y": 954, "w": 520, "h": 42},
+    "chronic_hepatitis": {"x": BAR_X, "y": 996, "w": 520, "h": 42},
 
-    "respiratory_disorders": {"x": 430, "y": 1076, "w": 520, "h": 42},
-    "kidney_function": {"x": 430, "y": 1118, "w": 520, "h": 42},
-    "digestive_disorders": {"x": 430, "y": 1160, "w": 520, "h": 42},
+    "respiratory_disorders": {"x": BAR_X, "y": 1076, "w": 520, "h": 42},
+    "kidney_function": {"x": BAR_X, "y": 1118, "w": 520, "h": 42},
+    "digestive_disorders": {"x": BAR_X, "y": 1160, "w": 520, "h": 42},
 
-    "major_depression": {"x": 430, "y": 1270, "w": 520, "h": 42},
-    "adhd_learning": {"x": 430, "y": 1312, "w": 520, "h": 42},
-    "dopamine_decrease": {"x": 430, "y": 1354, "w": 520, "h": 42},
-    "serotonin_decrease": {"x": 430, "y": 1396, "w": 520, "h": 42},
+    "major_depression": {"x": BAR_X, "y": 1270, "w": 520, "h": 42},
+    "adhd_learning": {"x": BAR_X, "y": 1312, "w": 520, "h": 42},
+    "dopamine_decrease": {"x": BAR_X, "y": 1354, "w": 520, "h": 42},
+    "serotonin_decrease": {"x": BAR_X, "y": 1396, "w": 520, "h": 42},
 }
 
 
@@ -62,7 +64,7 @@ def extract_pages(file_bytes):
     page2 = np.array(pages[1])
     page3 = np.array(pages[2])
 
-    # remove top analyzer section
+    # crop analyzer section off the top
     page2 = page2[900:2500, :]
     page3 = page3[400:2000, :]
 
