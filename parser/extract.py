a67392e7cdbce2136c2153b41c3cf6d673127c00
grainbolt-max <grainbolt@gmail.com>
2026-03-13 06:20:51 -0500

    Update extract.py

--- parser/extract.py
+++ parser/extract.py
@@ -2,21 +2,21 @@ import cv2
 import numpy as np
 from pdf2image import convert_from_bytes
 
-ENGINE_NAME = "v78_locked_column_classifier"
+ENGINE_NAME = "v79_locked_column_classifier"
 
 # --------------------------------------------------
-# LOCKED SAMPLING COLUMN (confirmed correct)
+# LOCKED SAMPLING COLUMN
 # --------------------------------------------------
 
 X_LEFT = 939
 X_RIGHT = 951
 
-# Height of sampling block
+# height of the green sampling block
 BLOCK_HEIGHT = 16
 
 
 # --------------------------------------------------
-# DISEASE ROW START POSITIONS (from grid measurement)
+# ROW START COORDINATES (UPDATED FROM GRID)
 # --------------------------------------------------
 
 ROW_START = {
@@ -24,35 +24,35 @@ ROW_START = {
     # PANEL 1
     "large_artery_stiffness": 920,
     "peripheral_vessel": 950,
-    "blood_pressure_uncontrolled": 980,
-    "small_medium_artery_stiffness": 1010,
-    "atherosclerosis": 1040,
+    "blood_pressure_uncontrolled": 985,
+    "small_medium_artery_stiffness": 1020,
+    "atherosclerosis": 1055,
     "ldl_cholesterol": 1075,
     "lv_hypertrophy": 1110,
-    "metabolic_syndrome": 1175,
+    "metabolic_syndrome": 1170,
     "insulin_resistance": 1200,
-    "beta_cell_function_decreased": 1230,
+    "beta_cell_function_decreased": 1235,
     "blood_glucose_uncontrolled": 1265,
-    "tissue_inflammatory_process": 1290,
+    "tissue_inflammatory_process": 1305,
 
     # PANEL 2
-    "hypothyroidism": 1525,
+    "hypothyroidism": 1530,
     "hyperthyroidism": 1560,
-    "hepatic_fibrosis": 1585,
+    "hepatic_fibrosis": 1590,
     "chronic_hepatitis": 1625,
     "prostate_cancer": 1655,
-    "respiratory_disorders": 1680,
+    "respiratory_disorders": 1685,
     "kidney_function_disorders": 1715,
-    "digestive_disorders": 1750,
-    "major_depression": 1815,
+    "digestive_disorders": 1745,
+    "major_depression": 1805,
     "adhd_children_learning": 1840,
     "cerebral_dopamine_decreased": 1870,
-    "cerebral_serotonin_decreased": 1900,
+    "cerebral_serotonin_decreased": 1910,
 }
 
 
 # --------------------------------------------------
-# COLOR CLASSIFIER
+# COLOR CLASSIFICATION
 # --------------------------------------------------
 
 def classify_color(mean_bgr):
@@ -82,7 +82,7 @@ def extract_scores(pdf_bytes, debug=False):
 
     pages = convert_from_bytes(pdf_bytes, dpi=200)
 
-    # Disease panels are on page 2
+    # disease panels are on page 2
     img = np.array(pages[1])
 
     scores = {}
@@ -91,7 +91,7 @@ def extract_scores(pdf_bytes, debug=False):
 
     height = img.shape[0]
 
-    # Draw locked sampling column
+    # draw locked column
     if debug:
         cv2.rectangle(
             debug_img,
